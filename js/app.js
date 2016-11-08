16be12c3c431e753608b68ef9426d7e9b4220448
Jared Wasinger <j-wasinger@hotmail.com>
2016-11-07 23:02:56 -0500

    update website

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
   .controller("AppCtrl", function($scope) {
     $scope.currentNavItem = 'page1';
     $scope.templates = [
-      {url: "/templates/jared-wasinger.html", name:"About"},
-      {url: "/templates/jared-wasinger-resume.html", name:"Resume"},
+      {url: "/templates/jared-wasinger.html", name:"Home"},
+      //{url: "/templates/jared-wasinger-resume.html", name:"Resume"},
       {url: "/templates/jared-wasinger-projects.html", name:"Projects/Portfolio"},
     ]
   });
