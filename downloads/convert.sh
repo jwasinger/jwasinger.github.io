099b8c343081d926639e935bf934dacfc2927ee7
Jared Wasinger <j-wasinger@hotmail.com>
2018-06-05 17:40:25 -0700

    remove cruft.

--- downloads/convert.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-rm jared_wasinger_resume.pdf
-wkhtmltopdf jared_wasinger_resume.html jared_wasinger_resume.pdf
--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@
   .controller("AppCtrl", function($scope) {
     $scope.currentNavItem = 'page1';
     $scope.templates = [
-      {url: "/templates/jared-wasinger.html", name:"Home"},
+      {url: "/templates/jared-wasinger.html", name:"Home"}//,
       //{url: "/templates/jared-wasinger-resume.html", name:"Resume"},
-      {url: "/templates/jared-wasinger-projects.html", name:"Projects/Portfolio"},
+      //{url: "/templates/jared-wasinger-projects.html", name:"Projects/Portfolio"},
     ]
   });
 })();
